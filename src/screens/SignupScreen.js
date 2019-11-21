e84a8dd4396280f9697ef8b4c2843d792c7671ca
Nehal Udyavar <nehaludyavar@gmail.com>
2019-11-20 20:47:38 -0800

    Sign up screen UI

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button title="Sign in" onPress={() => navigation.navigate("Signin")} />
-      <Button
-        title="Main Flow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer />
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer />
+      <Spacer>
+        <Button title="Sign up" />
+      </Spacer>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 150
+  }
+});
 
 export default SignupScreen;
