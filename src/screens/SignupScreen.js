ba0a0a0015f355e3208f9d16b0b3c15c86f6d5eb
Nehal Udyavar <nehaludyavar@gmail.com>
2019-11-22 16:28:41 -0800

    Extract NavLink component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Text } from "react-native-elements";
 import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
 
@@ -16,13 +17,10 @@ const SignupScreen = ({ navigation }) => {
         errorMessage={state.errorMessage}
         onSubmit={signUp}
       />
-      <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-        <Spacer>
-          <Text style={styles.link}>
-            Already have an account? Sign in instead.
-          </Text>
-        </Spacer>
-      </TouchableOpacity>
+      <NavLink
+        text="Already have an account? Sign in instead"
+        routeName="Signin"
+      />
     </View>
   );
 };
@@ -38,9 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginBottom: 150
-  },
-  link: {
-    color: "blue"
   }
 });
 
