76d9bbfeb2e2c88bb1cffdb5c6e09d611e8346f0
Nehal Udyavar <nehaludyavar@gmail.com>
2019-12-17 16:30:47 +0400

    mocking location updates per second

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context as LocationContext } from "../context/LocationContext";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
@@ -11,6 +12,7 @@ import Map from "../components/Map";
 import "../_mockLocation";
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -23,7 +25,7 @@ const TrackCreateScreen = () => {
           distanceInterval: 10
         },
         location => {
-          console.log(location);
+          addLocation(location);
         }
       );
     } catch (err) {
