151c083288524aa205f6fd8e1ac1a6d7793d5b98
Nehal Udyavar <nehaludyavar@gmail.com>
2019-12-17 15:06:05 +0400

    Init. Location Context and Provider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
 import { setNavigator } from "./src/navigationRef";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 import AccountScreen from "./src/screens/AccountScreen";
@@ -32,8 +33,10 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App ref={navigator => setNavigator(navigator)} />
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App ref={navigator => setNavigator(navigator)} />
+      </AuthProvider>
+    </LocationProvider>
   );
 };
