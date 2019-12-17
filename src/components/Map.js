364ecb6a5f791956530e3db9b0fbd1b952e34a33
Nehal Udyavar <nehaludyavar@gmail.com>
2019-12-17 16:34:09 +0400

    Add position indicator on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Text, StyleSheet, ActivityIndicator } from "react-native";
 import { Context as LocationContext } from "../context/LocationContext";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 
 const Map = () => {
   const {
@@ -25,7 +25,14 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-    ></MapView>
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
