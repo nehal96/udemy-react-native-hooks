c8f2374bc805e679f4565c83ffcdc72ec282e4e6
Nehal Udyavar <nehaludyavar@gmail.com>
2019-12-18 10:36:04 +0400

    Automatic authentication through axios instance (add Auth header)

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from "axios";
+import { AsyncStorage } from "react-native";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "https://af18641c.ngrok.io"
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
